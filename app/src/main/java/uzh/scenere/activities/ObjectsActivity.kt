fc9fbaaf04b915ee76297719e5293c71fb95a7d3
Koshiro <marcobonzanigo@gmail.com>
2019-01-21 08:53:05 +0100

    GIT Add

---
+++ app/src/main/java/uzh/scenere/activities/ObjectsActivity.kt
@@ -0,0 +1,157 @@
+package uzh.scenere.activities
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import kotlinx.android.synthetic.main.scroll_holder.*
+import uzh.scenere.R
+import uzh.scenere.const.Constants
+import uzh.scenere.const.Constants.Companion.BUNDLE_SCENARIO
+import uzh.scenere.datamodel.Attribute
+import uzh.scenere.datamodel.Object
+import uzh.scenere.datamodel.Scenario
+import uzh.scenere.helpers.DatabaseHelper
+import uzh.scenere.helpers.StringHelper
+import uzh.scenere.helpers.getStringValue
+import uzh.scenere.views.SwipeButton
+import uzh.scenere.views.SwipeButton.SwipeButtonExecution
+
+
+class ObjectsActivity : AbstractManagementActivity() {
+
+    override fun getConfiguredInfoString(): Int {
+        return R.string.icon_explain_objects
+    }
+    override fun getConfiguredLayout(): Int {
+        return R.layout.activity_objects
+    }
+
+    enum class ObjectMode{
+        VIEW, EDIT_CREATE, ATTRIBUTES
+    }
+    private var objectsMode: ObjectMode = ObjectMode.VIEW
+    override fun isInViewMode(): Boolean {
+        return objectsMode == ObjectMode.VIEW
+    }
+    override fun isInEditMode(): Boolean {
+        return objectsMode == ObjectMode.EDIT_CREATE
+    }
+    override fun resetEditMode() {
+        activeObject = null
+        objectsMode = ObjectMode.VIEW
+    }
+
+    private val inputLabelName = "Object Name"
+    private val inputLabelDescription = "Object Description"
+    private var activeScenario: Scenario? = null
+    private var activeObject: Object? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activeScenario = intent.getSerializableExtra(BUNDLE_SCENARIO) as Scenario
+        creationButton =
+                SwipeButton(this,"Create New Object")
+                        .setButtonMode(SwipeButton.SwipeButtonMode.DOUBLE)
+                        .setColors(Color.WHITE,Color.GRAY)
+                        .setButtonStates(false,true,false,false)
+                        .setButtonIcons(R.string.icon_null,R.string.icon_edit,null,null,R.string.icon_object)
+                        .updateViews(true )
+        creationButton!!.setExecutable(generateCreationExecutable(creationButton!!))
+        holder_linear_layout_holder.addView(creationButton)
+        createTitle("",holder_linear_layout_holder)
+        for (obj in DatabaseHelper.getInstance(applicationContext).readBulk(Object::class, activeScenario)){
+            addObjectToList(obj)
+        }
+        holder_text_info_title.text = StringHelper.styleString(getSpannedStringFromId(R.string.icon_explain_objects),fontAwesome)
+        customizeToolbarText(resources.getText(R.string.icon_back).toString(),null,getLockIcon(),null,null)
+    }
+
+    private fun addObjectToList(obj: Object) {
+        val swipeButton = SwipeButton(this, obj.name)
+                .setColors(Color.WHITE, Color.GRAY)
+                .setButtonMode(SwipeButton.SwipeButtonMode.QUADRUPLE)
+                .setButtonIcons(R.string.icon_delete, R.string.icon_edit, R.string.icon_info, R.string.icon_null, null)
+                .setButtonStates(lockState == LockState.UNLOCKED, true, true, false)
+                .updateViews(true)
+        swipeButton.dataObject = obj
+        swipeButton.setCounter(DatabaseHelper.getInstance(applicationContext).readBulk(Attribute::class,obj).size,null)
+        swipeButton.setExecutable(generateObjectExecutable(swipeButton, obj))
+        holder_linear_layout_holder.addView(swipeButton)
+    }
+
+    private fun generateCreationExecutable(button: SwipeButton, obj: Object? = null): SwipeButtonExecution {
+        return object: SwipeButtonExecution{
+            override fun execRight() {
+                activeButton = button
+                openInput(ObjectMode.EDIT_CREATE)
+            }
+        }
+    }
+
+    private fun generateObjectExecutable(button: SwipeButton, obj: Object? = null): SwipeButtonExecution {
+        return object: SwipeButtonExecution{
+            override fun execLeft() {
+                if (obj!=null){
+                    removeObject(obj)
+                    showDeletionConfirmation(obj.name)
+                }
+            }
+            override fun execRight() {
+                activeButton = button
+                openInput(ObjectMode.EDIT_CREATE,obj)
+            }
+            override fun execUp() {
+                activeButton = button
+                openInput(ObjectMode.ATTRIBUTES,obj)
+            }
+            override fun execReset() {
+                resetEditMode()
+            }
+        }
+    }
+
+    override fun createEntity() {
+        val name = inputMap[inputLabelName]!!.getStringValue()
+        val introduction = inputMap[inputLabelDescription]!!.getStringValue()
+        val objectBuilder = Object.ObjectBuilder(activeScenario!!,name, introduction)
+        if (activeObject != null){
+            removeObject(activeObject!!)
+            objectBuilder.copyId(activeObject!!)
+        }
+        val obj = objectBuilder.build()
+        DatabaseHelper.getInstance(applicationContext).write(obj.id,obj)
+        addObjectToList(obj)
+    }
+
+    private fun openInput(objectsMode: ObjectMode, obj: Object? = null) {
+        activeObject = obj
+        this.objectsMode = objectsMode
+        when(objectsMode){
+            ObjectMode.VIEW -> {}//NOP
+            ObjectMode.EDIT_CREATE -> {
+                cleanInfoHolder(if (activeObject==null) getString(R.string.objects_create) else getString(R.string.objects_edit))
+                holder_text_info_content_wrap.addView(createLine(inputLabelName,false, obj?.name))
+                holder_text_info_content_wrap.addView(createLine(inputLabelDescription, true, obj?.description))
+            }
+            ObjectMode.ATTRIBUTES -> {
+                val intent = Intent(this, AttributesActivity::class.java)
+                intent.putExtra(Constants.BUNDLE_OBJECT, activeObject)
+                startActivity(intent)
+                return
+            }
+        }
+        execMorphInfoBar(InfoState.MAXIMIZED)
+    }
+
+    private fun removeObject(obj: Object) {
+        for (viewPointer in 0 until holder_linear_layout_holder.childCount){
+            if (holder_linear_layout_holder.getChildAt(viewPointer) is SwipeButton &&
+                    (holder_linear_layout_holder.getChildAt(viewPointer) as SwipeButton).dataObject == obj){
+                holder_linear_layout_holder.removeViewAt(viewPointer)
+                DatabaseHelper.getInstance(applicationContext).delete(obj.id)
+                return
+            }
+        }
+    }
+}
---
+++ app/src/main/java/uzh/scenere/helpers/DatabaseHelper.kt
@@ -0,0 +1,242 @@
+package uzh.scenere.helpers
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Base64
+import uzh.scenere.const.Constants
+import uzh.scenere.const.Constants.Companion.ATTRIBUTE_UID_IDENTIFIER
+import uzh.scenere.const.Constants.Companion.OBJECT_UID_IDENTIFIER
+import uzh.scenere.const.Constants.Companion.PROJECT_UID_IDENTIFIER
+import uzh.scenere.const.Constants.Companion.SCENARIO_UID_IDENTIFIER
+import uzh.scenere.const.Constants.Companion.STAKEHOLDER_UID_IDENTIFIER
+import uzh.scenere.datamodel.*
+import uzh.scenere.datamodel.database.SreDatabase
+import java.io.Serializable
+import kotlin.reflect.KClass
+
+class DatabaseHelper private constructor(context: Context) {
+    enum class DataMode{
+        PREFERENCES, DATABASE
+    }
+    private var mode: DataMode = DataMode.PREFERENCES
+    private var database: SreDatabase? = null
+    private var sharedPreferences: SharedPreferences = context.getSharedPreferences(Constants.SHARED_PREFERENCES, Context.MODE_PRIVATE)
+
+    init {
+        if (PermissionHelper.check(context, PermissionHelper.Companion.PermissionGroups.STORAGE)){
+            database = SreDatabase.getInstance(context)
+            mode = DataMode.DATABASE
+        }else{
+            mode = DataMode.PREFERENCES
+        }
+    }
+
+    companion object {
+        // Volatile: writes to this field are immediately made visible to other threads.
+        @Volatile
+        private var instance: DatabaseHelper? = null
+
+        fun getInstance(context: Context): DatabaseHelper {
+            return when {
+                instance != null -> instance!!
+                else -> synchronized(this) {
+                    if (instance == null) {
+                        instance = DatabaseHelper(context)
+                    }
+                    instance!!
+                }
+            }
+        }
+    }
+
+    public fun write(key: String, obj: Any, internalMode: DataMode = mode){
+        when(internalMode){
+            DataMode.PREFERENCES -> {
+                if (obj is Boolean) sharedPreferences.edit().putBoolean(key,obj).apply()
+                if (obj is String)  sharedPreferences.edit().putString(key,obj).apply()
+                if (obj is ByteArray)  sharedPreferences.edit().putString(key, Base64.encodeToString(obj, Base64.DEFAULT)).apply()
+                if (obj is Short)  sharedPreferences.edit().putInt(key,obj.toInt()).apply()
+                if (obj is Int)  sharedPreferences.edit().putInt(key,obj).apply()
+                if (obj is Long)  sharedPreferences.edit().putLong(key,obj).apply()
+                if (obj is Float)  sharedPreferences.edit().putFloat(key,obj).apply()
+                if (obj is Double)  sharedPreferences.edit().putLong(key, java.lang.Double.doubleToLongBits(obj)).apply()
+                if (obj is Project)  write(PROJECT_UID_IDENTIFIER+key,DataHelper.toByteArray(obj))
+                if (obj is Stakeholder)  write(STAKEHOLDER_UID_IDENTIFIER+key,DataHelper.toByteArray(obj))
+                if (obj is Object)  write(OBJECT_UID_IDENTIFIER+key,DataHelper.toByteArray(obj))
+                if (obj is Attribute)  write(ATTRIBUTE_UID_IDENTIFIER+key,DataHelper.toByteArray(obj))
+                if (obj is Scenario)  write(SCENARIO_UID_IDENTIFIER+key,DataHelper.toByteArray(obj))
+            }
+            DataMode.DATABASE -> {
+                if (obj is Boolean) database!!.writeBoolean(key,obj)
+                if (obj is String)  database!!.writeString(key,obj)
+                if (obj is ByteArray)  database!!.writeByteArray(key, obj)
+                if (obj is Short)  database!!.writeShort(key,obj)
+                if (obj is Int)  database!!.writeInt(key,obj)
+                if (obj is Long)  database!!.writeLong(key,obj)
+                if (obj is Float)  database!!.writeFloat(key,obj)
+                if (obj is Double)  database!!.writeDouble(key,obj)
+                if (obj is Project) database!!.writeProject(obj)
+                if (obj is Stakeholder) database!!.writeStakeholder(obj)
+                if (obj is Object) database!!.writeObject(obj)
+                if (obj is Attribute) database!!.writeAttribute(obj)
+                if (obj is Scenario) database!!.writeScenario(obj)
+            }
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    public fun <T : Serializable> read(key: String, clz: KClass<T>, valIfNull: T, internalMode: DataMode = mode): T? {
+        when(internalMode){
+            DataMode.PREFERENCES -> {
+                if (String::class == clz) return sharedPreferences.getString(key,valIfNull as String) as T?
+                if (ByteArray::class == clz){
+                    val byteArrayString: String = sharedPreferences.getString(key, "")
+                    if (StringHelper.hasText(byteArrayString)){
+                        return  Base64.decode(byteArrayString, Base64.DEFAULT) as T?
+                    }
+                    return valIfNull
+                }
+                if (Boolean::class == clz) return sharedPreferences.getBoolean(key,valIfNull as Boolean) as T?
+                if (Short::class == clz) {
+                    val intValue = sharedPreferences.getInt(key, 0)
+                    return if (intValue == 0) valIfNull else intValue.toShort() as T?
+                }
+                if (Int::class == clz) return sharedPreferences.getInt(key,valIfNull as Int) as T?
+                if (Long::class == clz) return sharedPreferences.getLong(key,valIfNull as Long) as T?
+                if (Float::class == clz) return sharedPreferences.getFloat(key,valIfNull as Float) as T?
+                if (Double::class == clz){
+                    val rawLongBits: Long = sharedPreferences.getLong(key, 0L)
+                    return if (rawLongBits == 0L) valIfNull else java.lang.Double.longBitsToDouble(rawLongBits) as T?
+                }
+                if (Project::class == clz) return readBinary(key, clz, PROJECT_UID_IDENTIFIER, valIfNull)
+                if (Stakeholder::class == clz) return readBinary(key, clz, STAKEHOLDER_UID_IDENTIFIER, valIfNull)
+                if (Object::class == clz) return readBinary(key, clz, OBJECT_UID_IDENTIFIER, valIfNull)
+                if (Attribute::class == clz) return readBinary(key, clz, ATTRIBUTE_UID_IDENTIFIER, valIfNull)
+                if (Scenario::class == clz) return readBinary(key, clz, SCENARIO_UID_IDENTIFIER, valIfNull)
+            }
+            DataMode.DATABASE -> {
+                if (Boolean::class == clz) return database!!.readBoolean(key, valIfNull as Boolean) as T?
+                if (String::class == clz) return database!!.readString(key,valIfNull as String) as T?
+                if (ByteArray::class == clz) return database!!.readByteArray(key, valIfNull as ByteArray) as T?
+                if (Short::class == clz) return database!!.readShort(key,valIfNull as Short) as T?
+                if (Int::class == clz) return database!!.readInt(key,valIfNull as Int) as T?
+                if (Long::class == clz) return database!!.readLong(key,valIfNull as Long) as T?
+                if (Float::class == clz) return database!!.readFloat(key,valIfNull as Float) as T?
+                if (Double::class == clz) return database!!.readDouble(key,valIfNull as Double) as T?
+                if (Project::class == clz) return database!!.readProject(key,valIfNull as Project) as T?
+                if (Stakeholder::class == clz) return database!!.readStakeholder(key,valIfNull as Stakeholder) as T?
+                if (Object::class == clz) return database!!.readObject(key,valIfNull as Object) as T?
+                if (Attribute::class == clz) return database!!.readAttribute(key,valIfNull as Attribute) as T?
+                if (Scenario::class == clz) return database!!.readScenario(key,valIfNull as Scenario) as T?
+            }
+        }
+        return null
+    }
+
+    private fun <T : Serializable> readBinary(key: String, clz: KClass<T>, identifier: String, valIfNull: T): T {
+        val bytes = read(identifier + key, ByteArray::class, ByteArray(0))
+        if (bytes != null && bytes.isNotEmpty()) {
+            val project = DataHelper.toObject(bytes, clz)
+            return project ?: valIfNull
+        }
+        return valIfNull
+    }
+
+    public fun <T : Serializable> readAndMigrate(key: String, clz: KClass<T>, valIfNull: T, deleteInPrefs: Boolean = true): T? {
+        when(mode){
+            DataMode.PREFERENCES -> {
+                return read(key,clz,valIfNull)
+            }
+            DataMode.DATABASE -> {
+                val readPref = read(key, clz, valIfNull, DataMode.PREFERENCES)
+                val readDb = read(key, clz, valIfNull, DataMode.DATABASE)
+                if (readPref == null || readPref == valIfNull){
+                    //Val was not saved, return DB value
+                    return readDb
+                }
+                //Delete from Prefs if not specified otherwise
+                if (deleteInPrefs){
+                    delete(key, DataMode.PREFERENCES)
+                }
+                //Value was saved, check DB value
+                if (readDb == null ||readDb == valIfNull){
+                    //DB value does'nt exist, write to DB, return
+                    write(key, readPref)
+                    return readPref
+                }
+                //DB value did exist, return
+                return readDb
+            }
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    public fun <T : Serializable> readBulk(clz: KClass<T>, key: Any? = null, internalMode: DataMode = mode): List<T> {
+        when(internalMode){
+            DataMode.PREFERENCES -> {
+                if (Project::class == clz) return readBulkInternal(clz, PROJECT_UID_IDENTIFIER)
+                if (Stakeholder::class == clz) return readBulkInternal(clz, STAKEHOLDER_UID_IDENTIFIER)
+                if (Object::class == clz) return readBulkInternal(clz, OBJECT_UID_IDENTIFIER)
+                if (Attribute::class == clz) return readBulkInternal(clz, ATTRIBUTE_UID_IDENTIFIER)
+                if (Scenario::class == clz) return readBulkInternal(clz, SCENARIO_UID_IDENTIFIER)
+            }
+            DataMode.DATABASE -> {
+                if (Project::class == clz) return database!!.readProjects() as List<T>
+                if (Stakeholder::class == clz && key is Project) return database!!.readStakeholder(key) as List<T>
+                if (Object::class == clz && key is Scenario) return database!!.readObjects(key) as List<T>
+                if (Attribute::class == clz && key is String) return database!!.readAttributes(key) as List<T>
+                if (Scenario::class == clz && key is Project) return database!!.readScenarios(key) as List<T>
+            }
+        }
+        return emptyList()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <T : Serializable> readBulkInternal(clz: KClass<T>, identifier: String): List<T> {
+        val list = ArrayList<Serializable>()
+        for (entry in sharedPreferences.all) {
+            if (entry.key.contains(identifier)) {
+                val bytes = Base64.decode((entry.value as String), Base64.DEFAULT)
+                list.add(DataHelper.toObject(bytes, clz)!!)
+            }
+        }
+        return list as List<T>
+    }
+
+    public fun delete(key: String, internalMode: DataMode = mode) {
+        when(internalMode){
+            DataMode.PREFERENCES -> {
+                sharedPreferences.edit().remove(key).apply()
+                sharedPreferences.edit().remove(PROJECT_UID_IDENTIFIER+key).apply()
+            }
+            DataMode.DATABASE -> {
+                database!!.deleteData(key)
+                database!!.deleteNumber(key)
+                database!!.deleteString(key)
+                database!!.deleteProject(key)
+                database!!.deleteStakeholder(key)
+                database!!.deleteObject(key)
+                database!!.deleteAttribute(key)
+                database!!.deleteScenario(key)
+            }
+        }
+    }
+
+    fun clear() {
+        when(mode){
+            DataMode.PREFERENCES -> {
+                sharedPreferences.edit().clear().apply()
+            }
+            DataMode.DATABASE -> {
+                database!!.truncateData()
+                database!!.truncateNumbers()
+                database!!.truncateStrings()
+                database!!.truncateProjects()
+                database!!.truncateStakeholders()
+                database!!.truncateObjects()
+                database!!.truncateAttributes()
+                database!!.truncateScenarios()
+            }
+        }
+    }
+}
